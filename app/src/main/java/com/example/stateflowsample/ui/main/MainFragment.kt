2774b52f666e5f3e33d851ddadcbc9e612eca3e8
Yuki Nishimura <yukinkopen@gmail.com>
2022-12-04 18:13:50 +0900

    StateFlowを実装

--- app/src/main/java/com/example/stateflowsample/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/stateflowsample/ui/main/MainFragment.kt
@@ -6,8 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.stateflowsample.R
 import com.example.stateflowsample.databinding.FragmentMainBinding
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 class MainFragment : Fragment() {
 
@@ -29,6 +34,18 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(inflater)
+
+        binding.button.setOnClickListener {
+            viewModel.countUp()
+        }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.map { it.count }.distinctUntilChanged().onEach {
+                    binding.countNum.text = it.toString()
+                }.launchIn(this)
+            }
+        }
         return binding.root
     }
 }
--- app/src/main/java/com/example/stateflowsample/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/stateflowsample/ui/main/MainViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.stateflowsample.ui.main
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _uiState
+
+    data class UiState(
+        val count: Int = 0
+    )
+
+    fun countUp() {
+        _uiState.update { it.copy(count = uiState.value.count + 1) }
+    }
 }
