d7e00d4e5bd79dc226a7b24ca92ab547b5583dd7
Yuki Nishimura <yukinkopen@gmail.com>
2022-12-04 17:17:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/stateflowsample/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.stateflowsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stateflowsample.databinding.ActivityMainBinding
+import com.example.stateflowsample.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/stateflowsample/ui/main/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.example.stateflowsample.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.stateflowsample.R
+import com.example.stateflowsample.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater)
+        return binding.root
+    }
+}
